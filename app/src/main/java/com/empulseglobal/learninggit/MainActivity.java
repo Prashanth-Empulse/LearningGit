c82e3b14f06eedddccd484aa1119c209d8bda501
Prashanth-Empulse <prashanth.ks@empulseglobal.com>
2019-04-26 00:36:19 +0530

    Third Commit From Empulse Laptop

--- app/src/main/java/com/empulseglobal/learninggit/MainActivity.java
+++ app/src/main/java/com/empulseglobal/learninggit/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //To make Second commit
-        Log.d("To commit","Siteurl Laptop Commit");
+        Log.d("To commit","Empulse Laptop Commit");
 
     }
 }
